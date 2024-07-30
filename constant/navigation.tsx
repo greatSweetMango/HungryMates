5f96d156cda34bef0ea26495c91742a6a9fb4281
kimjaehyuk <jhkim0097@gmail.com>
2024-07-30 23:49:29 +0900

    feat logout

--- constant/navigation.tsx
+++ constant/navigation.tsx
@@ -1,34 +1,39 @@
-import { ArrowLeft, Home, MessageCircleMore, ShoppingBag, User } from "lucide-react"
-
-const navStyle = `text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0`
-
-export const sidebarLinks = [
+import {
+    ArrowLeft,
+    Home,
+    MessageCircleMore,
+    ShoppingBag,
+    User,
+  } from "lucide-react";
+  import { signOut } from "next-auth/react";
+  
+  const navStyle = `text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0`;
+  
+  export const sidebarLinks = [
     {
-        label: "Home",
-        href: "/home",
-        icon: (
-            <Home className={`${navStyle}`} />
-        )
+      label: "Home",
+      href: "/home",
+      icon: <Home className={`${navStyle}`} />,
     },
     {
-        label: "Threads",
-        href: "/threads",
-        icon: (
-            <MessageCircleMore className={`${navStyle}`} />
-        )
+      label: "Threads",
+      href: "/threads",
+      icon: <MessageCircleMore className={`${navStyle}`} />,
     },
     {
-        label: "Orders",
-        href: "/orders",
-        icon: (
-            <ShoppingBag className={`${navStyle}`} />
-        )
+      label: "Orders",
+      href: "/orders",
+      icon: <ShoppingBag className={`${navStyle}`} />,
     },
     {
-        label: "Logout",
-        href: "/",
-        icon: (
-            <ArrowLeft className={`${navStyle}`} />
-        )
+      label: "Logout",
+      href: "#",
+      icon: (
+        <ArrowLeft
+          onClick={() => signOut({ callbackUrl: "/" })}
+          className={`${navStyle}`}
+        />
+      ),
     },
-]
+  ];
+  
